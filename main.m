a44d14cba3080afe332eebb16e1292b7957ad72f
MaiBendary <mai.bendary23@gmail.com>
2023-05-06 13:59:06 +0300

    Implement Ideal LPF

--- main.m
+++ main.m
@@ -21,6 +21,19 @@ subplot(3,1,1)
 plot(fvec,ECG_Signal_f_mg)
 title('ECG signal in frequency domain')
 
+%--------Ideal LBF--------%
+fc=0.5;
+index = find(fvec<=fc & fvec>=0);
+ECG_Signal_f([index]) = 0;
+% remove frequencies < -fc (remove negative HSB)
+index2 = find(fvec>=(-fc) & fvec<=0);
+ECG_Signal_f(index2) = 0;
+ECG_Signal_f_mg = abs(ECG_Signal_f);
+
+subplot(3,1,2)
+plot(fvec,ECG_Signal_f_mg)
+title('ECG signal in frequency domain after LBF at 0.5 HZ')
+
 
 
 
